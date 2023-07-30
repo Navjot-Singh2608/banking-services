8cbb004a058d1565cb085ecc63f34be0de286840
navjotsingh2608 <navjot@argenelec.com>
2023-07-30 00:31:53 -0400

    update

---
+++ src/test/java/com/banking/bankingservices/BankingServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.bankingservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
